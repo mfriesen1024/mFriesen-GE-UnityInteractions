52cab45e913b7811c3f6d67641dc190ad1b3e8e8
Matthew Friesen <144160470+mfriesen1024@users.noreply.github.com>
2024-03-19 10:07:23 -0300

    Citing sources good.

--- Assets/Credits_References.cs
+++ Assets/Credits_References.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-
-// Brackeys Tutorial https://www.youtube.com/watch?v=whzomFgjT50
+﻿// Brackeys Tutorial https://www.youtube.com/watch?v=whzomFgjT50
 // Tiny RPG Forest Pack https://assetstore.unity.com/packages/2d/characters/tiny-rpg-forest-114685
 // Hundred Fires Games https://www.youtube.com/watch?v=VsSq_Ispo3Q
+// GameplayInteraction https://github.com/SamR77/GameEngProgI_GameplayInteraction-Student
